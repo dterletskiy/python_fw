ffbe1917bc7b35e57540779e6c3872b0f3eb5612
Dmytro Terletskyi <dmytro_terletskyi@epam.com>
2023-12-07 23:53:50 +0200

    linux.network.ping:
    Removed default ip.
    Remove sudo auto add.

--- pfw/linux/network/ping.py
+++ pfw/linux/network/ping.py
@@ -3,20 +3,18 @@ import pfw.shell
 
 
 
-def ping( **kwargs ):
-   kw_ip = kwargs.get( "ip", "8.8.8.8" )
+def ping( ip: str, **kwargs ):
    kw_interval = kwargs.get( "interval", None )
    kw_count = kwargs.get( "count", None )
    kw_namespace = kwargs.get( "namespace", None )
 
    kwargs[ "output" ] = kwargs.get( "output", pfw.shell.eOutput.PTY )
-   kwargs[ "sudo" ] = None != kw_namespace or kwargs.get( "sudo", False )
 
    command = ""
    command += f"ip netns exec {kw_namespace}" if kw_namespace else ""
    command += " ping"
    command += f" -i {kw_interval}" if kw_interval else ""
    command += f" -c {kw_count}" if kw_count else ""
-   command += f" {kw_ip}"
+   command += f" {ip}"
    return pfw.shell.execute( command, **kwargs )
 # def ping
