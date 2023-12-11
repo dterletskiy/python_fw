3c15f4041bda813ba67d16d33250c9dcba6848a1
Dmytro Terletskyi <dmytro_terletskyi@epam.com>
2023-12-11 13:51:05 +0200

    linux.network.ping: Extended function parameters by "interface" parameter.

--- pfw/linux/network/ping.py
+++ pfw/linux/network/ping.py
@@ -7,6 +7,7 @@ def ping( ip: str, **kwargs ):
    kw_interval = kwargs.get( "interval", None )
    kw_count = kwargs.get( "count", None )
    kw_namespace = kwargs.get( "namespace", None )
+   kw_interface = kwargs.get( "interface", None )
 
    kwargs[ "output" ] = kwargs.get( "output", pfw.shell.eOutput.PTY )
 
@@ -15,6 +16,7 @@ def ping( ip: str, **kwargs ):
    command += " ping"
    command += f" -i {kw_interval}" if kw_interval else ""
    command += f" -c {kw_count}" if kw_count else ""
+   command += f" -I {kw_interface}" if kw_interface else ""
    command += f" {ip}"
    return pfw.shell.execute( command, **kwargs )
 # def ping
